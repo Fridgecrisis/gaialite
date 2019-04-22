fe0cb35232c0e10bc2f81e542402df0227184144
Fridgecrisis <fridgecrisis@gmail.com>
2019-04-21 20:31:31 -0600

    Battlers can attack! They can die! There is a bar that shows the current battler's remaining energy! They turn in a certain direction to attack and when they're not moving! I'm thinking eventually this will be a left-or-right thing, not just one way, but this is good enough for now.

---
+++ Gaialite Gameplay Prototype/Assets/SetActionFlags.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActionFlag : MonoBehaviour {
+
+	void ActionStarted() {
+		var animator = gameObject.GetComponent<Animator>();
+		animator.SetBool("Action", true);
+	}
+	
+	void ActionFinished() {
+		var animator = gameObject.GetComponent<Animator>();
+		animator.SetBool("Action", false);
+	}
+	
+}
