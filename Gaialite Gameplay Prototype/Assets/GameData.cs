10669d76d909fc0825fa592f7bd1ad0ddce6648f
Fridgecrisis <fridgecrisis@gmail.com>
2019-06-13 18:57:18 -0600

    Added visual menus and buttons you can click to do the things. Started working on some ways to keep track of game progress and load/unload levels.

---
+++ Gaialite Gameplay Prototype/Assets/GameData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+
+	public string currentMapName;
+	public string nextMapName;
+	
+	public bool seenIntro;
+
+	void Start () {
+	}
+	
+	void Update () {
+	}
+
+/* 	public Dictionary<string, bool> gameProgress;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+	
+	void Initialize () {
+		AddProgressItem("Seen intro", false);
+	}
+	
+	void AddProgressItem (string name, bool progressValue) {
+		if (!gameProgress.ContainsKey(name)) {
+			gameProgress.Add(name, progressValue);
+		}
+	} */
+}
